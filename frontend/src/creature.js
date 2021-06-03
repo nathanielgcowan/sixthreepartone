f790005569c14e1c5c29073846916b0461db45ee
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-03 12:22:58 -0500

    deck to card

--- frontend/src/creature.js
+++ frontend/src/creature.js
@@ -1,1 +1,13 @@
-console.log("hello")
+class Creature {
+    static allCreatures = []
+
+    constructor(creature){
+        this.id = creature.id
+        this.name = creature.attributes.name
+        this.image = creature.attributes.image
+        this.description = creature.attributes.description
+        this.skills = creature.attributes.skills
+        Creature.allCreatures.push(this)
+        this.renderCreatures()
+    }
+}
