9fbe7482e12702f25233101b1e261fb78d9b3e32
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-03 22:25:25 -0500

    redo the index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,8 @@ const creaturesURL = "http://localhost:3000/creatures"
 const skillsURL = "http://localhost:3000/skills"
 const makeACreature = document.querySelector(".makeacreature")
 const enterACreatureName = document.querySelector(".creaturename")
-const enterACreatureImage = document.querySelector(".creatureimage")
-const enterACreatureDescription = document.querySelector(".creaturedescription")
+const enterCreatureImage = document.querySelector(".createimage")
+const enterCreatureDescription = document.querySelector(".creaturedescription")
 const card = document.querySelector(".card")
 
 makeACreature.addEventListener("submit", Creature.submitCreature)
