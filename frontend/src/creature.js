cca130d74c1f6c4ee80f44359732eb551c98d439
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-03 22:21:44 -0500

    blanks

--- frontend/src/creature.js
+++ frontend/src/creature.js
@@ -1,36 +1,3 @@
 class Creature {
-
-    static allCreatures = []
-
-    constructor(creature){
-        this.id = creature.id
-        this.name = creature.attributes.name
-        this.image = creature.attributes.image
-        this.description = creature.attributes.description
-        this.skills = creature.attributes.skills
-        Creature.allCreatures.push(this)
-        this.renderCreature()
-    }
-
-    // Render Creatures
-    static renderCreatures(creatures){
-        card.innerHTML = ""
-        for (let c of creatures){
-            c.renderCreature()
-        }
-    }
-
-    // Fetch Creatures
-    static fetchCreatures(){
-        fetch(creaturesURL)
-        .then(res => res.json())
-        .then(c => {
-            for(let c of creatures.data){
-                let newCreatureCard = new Creature(c)
-            }
-        })
-    }
-
-    // Render The Creature
-
+    
 }
