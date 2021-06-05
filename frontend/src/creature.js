25918b40aaf3021956291413f6c3f48bc37f5074
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-04 23:47:23 -0500

    t

--- frontend/src/creature.js
+++ frontend/src/creature.js
@@ -1,3 +1,88 @@
 class Creature {
-    
+
+    static allCreatures = []
+
+    constructor(creature){
+        this.id = creature.id
+        this.name = creature.attributes.name
+        this.image = creature.attributes.image
+        this.description = creature.attributes.description
+        this.skills = creature.attributes.skills
+        Creature.allCreatures.push(this)
+        this.renderCreature()
+    }
+
+    static renderCreatures(creatures){
+        card.innerHTML = ""
+        for(let c of creatures){
+            c.renderCreature()
+        }
+    }
+
+    static fetchCreatures(){
+        fetch(creaturesURL)
+        .then(res => res.json())
+        .then(creatures => {
+            for(let c of creatures.data){
+                let newCreatureDeck = new Creature(c)
+            }
+        })
+    }
+
+    renderCreature(){
+        const creatureLi = document.createElement('li')
+        const h2 = document.createElement('h2')
+        const img = document.createElement('img')
+        const p = document.createElement('p')
+        const creaturesSkills = document.createElement('ul')
+        const deleteButton = document.createElement('button')
+        const skillForm = document.createElement('form')
+        creatureLi.dataset.id = this.id
+        // dataset - allows you to set and get your elements
+        // console.log(creatureLi.dataset.id)
+        card.appendChild(creatureLi)
+        h2.innerText = this.name
+        img.src = this.image
+        p.innerText = this.description
+        deleteButton.innerText = "Delete"
+        deleteButton.addEventListener("click", this.deleteCreature)
+        skillForm.innerHTML += `
+        Add Skill <input type="text">
+        <input type="submit">
+        `
+        skillForm.addEventListener("submit", Skill.createSkill)
+        creaturesSkills.dataset.id = this.id
+        this.skills.forEach(skill => {
+            let newSkill = new Skill(skill)
+            newSkill.renderSkill(creaturesSkills)
+        })
+        creatureLi.append(h2, img, p, skillForm, creaturesSkills, deleteButton)
+    }
+    static submitCreature(event){
+        event.preventDefault()
+        fetch(creaturesURL, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                "Accept":"application/json",
+            },
+            body: JSON.stringify({
+                name: enterCreatureName.value,
+                image: enterCreatureImage.value,
+                description: enterCreatureDescription.value
+            })
+        })
+        .then(response => response.json())
+        .then(creature => {
+            let newCreature = new Creature(creature.data)
+            makeACreature.reset()
+        })
+    }
+    deleteCreature(){
+        const creatureId = this.parentElement.dataset.id
+        fetch(`${creaturesURL}/${creatureId}`,{
+            method:"DELETE"
+        })
+        this.parentElement.remove()
+    }
 }
---
+++ frontend/src/skill.js
@@ -0,0 +1,58 @@
+class Skill{
+
+    constructor(skill){
+        this.id = skill.id
+        this.name = skill.name
+        this.creature_id = skill.creature_id
+    }
+
+    static createSkill(e){
+        e.preventDefault()
+        const li = document.createElement('li')
+        const skillName = e.target.children[0].value
+        const creaturesSkills = e.target.previousElementSibling
+        const creatureId = e.target.parentElement.dataset.id
+        Skill.submitSkill(skillName, creaturesSkills, creatureId)
+        e.target.reset()
+    }
+
+    renderSkill(creaturesSkills){
+        const li = document.createElement('li')
+        const deleteButton = document.createElement('button')
+        li.dataset.id = this.id
+        li.innerText = this.name
+        li.append(deleteButton)
+        deleteButton.addEventListener("click", this.deleteSkill)
+        creaturesSkills.appendChild(li)
+    }
+
+    static submitSkill(skillName, creaturesSkills, creatureId){
+        fetch(skillsURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                name: skillName,
+                creature_id: creatureId,
+                creatureSkills: creaturesSkills
+            })
+        })
+        .then(response => response.json())
+        .then(skill => {
+            let newSkill = new Skill(skill)
+            const creature = Creature.allCreatures.find(creature => parseInt(creature.id) === newSkill.creature_id)
+            creature.skills.push(newSkill)
+            newSkill.renderSkill(creaturesSkills)
+        })
+    }
+
+    deleteSkill(){
+        const skillId = this.parentElement.dataset.id
+        fetch(`${skillsURL}/${skillId}`,{
+            method:"DELETE"
+        })
+        this.parentElement.remove()
+    }
+}
